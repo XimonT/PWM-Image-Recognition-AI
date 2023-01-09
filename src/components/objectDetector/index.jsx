dff51629fc183ae0148a67ee3ba13af414060e1d
XimonT <46265269+XimonT@users.noreply.github.com>
2023-01-09 11:46:52 +0100

    Update index.jsx

--- src/components/objectDetector/index.jsx
+++ src/components/objectDetector/index.jsx
@@ -12,7 +12,7 @@ const ObjectDetectorContainer = styled.div`
 
 const DetectorContainer = styled.div`
   min-width: 200px;
-  height: 600px;
+  height: 500px;
   border: 3px solid #fff;
   border-radius: 5px;
   display: flex;
